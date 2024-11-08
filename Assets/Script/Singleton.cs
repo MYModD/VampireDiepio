c7f60940f11a9c9fad1ea7cddf5b3054787e6cae
Mona Lisa <20237086-miyamotoayumu@hcs.ac.jp>
2024-11-08 17:13:41 +0900

    Add

---
+++ Assets/Script/Singleton.cs
@@ -0,0 +1,52 @@
+using UnityEngine;
+
+public class Singleton<T> : MonoBehaviour where T : Component
+{
+#pragma warning disable IDE1006 // 命名スタイル
+    private static T instance;
+#pragma warning restore IDE1006 // 命名スタイル
+    public static T Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = (T)FindObjectOfType(typeof(T));
+                if (instance == null)
+                {
+                    SetupInstance();
+                }
+            }
+            return instance;
+        }
+    }
+    public virtual void Awake()
+    {
+        RemoveDuplicates();
+    }
+
+    private static void SetupInstance()
+    {
+        instance = (T)FindObjectOfType(typeof(T));
+        if (instance == null)
+        {
+            GameObject gameObj = new GameObject();
+            gameObj.name = typeof(T).Name;
+            instance = gameObj.AddComponent<T>();
+            DontDestroyOnLoad(gameObj);
+        }
+    }
+
+    private void RemoveDuplicates()
+    {
+        if (instance == null)
+        {
+            instance = this as T;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
