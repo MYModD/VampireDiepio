473389a3e082d42ef92fe832ef13b2b68fd742b5
Mona Lisa <20237086-miyamotoayumu@hcs.ac.jp>
2024-12-02 00:41:22 +0900

    ファイル整理

---
+++ Assets/Script/Debris/DebrisMove.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class DebrisMove : MonoBehaviour
+{
+    
+    void Awake()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
