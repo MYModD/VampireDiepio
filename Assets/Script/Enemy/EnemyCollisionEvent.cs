d6473a7306ae196a1f2ba13b2e3f3facd57d9d41
Mona Lisa <20237086-miyamotoayumu@hcs.ac.jp>
2024-12-11 14:30:52 +0900

    Add

---
+++ Assets/Script/Enemy/EnemyCollisionEvent.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class EnemyCollisionEvent : CollisionEventBase
+{
+
+    public override void OnCustomCollisionEnter(SimpleShapeCollider2D collision)
+    {
+        // Implement your logic here
+    }
+
+    public override void OnCustomCollisionStay(SimpleShapeCollider2D collision)
+    {
+        // Implement your logic here
+    }
+
+    public override void OnCustomCollisionExit(SimpleShapeCollider2D collision)
+    {
+        // Implement your logic here
+    }
+}
---
+++ Assets/Script/Enemy/EnemyMove.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class EnemyMove : MonoBehaviour
+{
+    
+    void Awake()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
